50d3d6a66a422354a55424eafd5b716c9fc7e665
Donggyu <nninnnin7@likelion.org>
2018-07-28 06:14:56 +0000

    좋아

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'menu/main'
+
+  get 'menu/mentoring'
+
+  get 'menu/ranking'
+
+  get 'menu/execution'
+  
+  root 'menu#main'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
