8a41b5ab0e49cdf6f68d665350ef87c36b2ee3d9
Jihyun Kim <adf0333@likelion.org>
2018-08-16 05:18:15 +0000

    dantodanti

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -6,10 +6,11 @@ class PublicController < ApplicationController
     
     def create
         @newrecord=Blacklist.new
-        @newrecord.title=params[:title] 
-        @newrecord.content=params[:content] 
+        @newrecord.name=params[:name] 
+        @newrecord.major=params[:major]
+        @newrecord.number=params[:number] 
         @newrecord.save
-        redirect_to "/public/show/#{@newrecord.id}"
+        redirect_to "/public/index"
     end
     
     def index
@@ -19,10 +20,19 @@ class PublicController < ApplicationController
     def show
         @show = params[:id]
         @findone = Blacklist.find(@show)
-        @s_title = @findone.title
-        @s_content = @findone.content
+        @s_name = @findone.name
+        @s_major = @findone.major
+        @s_number = @findone.number
         @comments = Comment.where(blacklist_id: params[:id])
     end
-        
+    
+    before_action :log_impression, :only=> [:show]
+ 
+   def log_impression
+      @hit_post = Blacklist.find(params[:id])
+      # this assumes you have a current_user method in your authentication system
+      #@hit_post.impressions.create(ip_address: request.remote_ip, user_id: current_user.id) # current_user 오류
+   end
+
     
 end
