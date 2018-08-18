8438b8dca6fb2ed9f896e3e3fa26f928a642ef68
Dajeong Shin <ssdj0607@likelion.org>
2018-08-18 04:35:43 +0000

    dajeong0818

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180813131832) do
+ActiveRecord::Schema.define(version: 20180818042136) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "name"
@@ -22,9 +22,8 @@ ActiveRecord::Schema.define(version: 20180813131832) do
   end
 
   create_table "blacklists", force: :cascade do |t|
-    t.string   "name"
-    t.string   "major"
-    t.string   "number"
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -87,6 +86,7 @@ ActiveRecord::Schema.define(version: 20180813131832) do
     t.string   "userdepartment"
     t.string   "usernumber"
     t.string   "userage"
+    t.integer  "like"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
